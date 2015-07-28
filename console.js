54c7e89d089fa7356363ecef8c97cd4150875c62
Valérian Saliou <valerian@valeriansaliou.name>
2015-07-28 20:30:28 +0200

    fix
    Signed-off-by: Valérian Saliou <valerian@valeriansaliou.name>

--- console.js
+++ console.js
@@ -15,6 +15,7 @@ var Console = (function () {
 
 
   /* Variables */
+
   self._html_sel = document.getElementsByTagName('html')[0];
   self._environment = self._html_sel.getAttribute('data-environment');
   self._development = self._environment == 'development';
@@ -24,68 +25,40 @@ var Console = (function () {
 
 
   /* Methods */
-  self._warn = function (ns, value) {
+
+  self.warn = function (ns, value) {
     if (self._has) {
       console.warn(ns, value);
     }
   };
 
-  self._error = function (ns, value) {
+  self.error = function (ns, value) {
     if (self._has) {
       console.error(ns, value);
     }
   };
 
-  self._info = function (ns, value) {
+  self.info = function (ns, value) {
     if (self._has) {
       console.info(ns, value);
     }
   };
 
-  self._log = function (ns, value) {
+  self.log = function (ns, value) {
     if (self._has) {
       console.log(ns, value);
     }
   };
 
-  self._debug = function (ns, value) {
+  self.debug = function (ns, value) {
     if (self._has) {
-      console.debug(ns, value);
-    }
-  };
-
-
-  /* Adapters */
-  self._adapter = function (level, ns, value) {
-    var adapter = null;
-
-    try {
-      switch (level) {
-        case 0:
-          adapter = self._warn;  break;
-        case 1:
-          adapter = self._error; break;
-        case 2:
-          adapter = self._info;  break;
-        case 3:
-          adapter = self._log;   break;
-        case 4:
-          adapter = self._debug; break;
+      if (typeof console.debug != "undefined") {
+        console.debug(ns, value);
+      } else {
+        console.log(ns, value);
       }
-    } catch (e) {
-      adapter = function() {};
     }
-
-    return adapter;
   };
-
-
-  /* Methods */
-  self.warn  = self._adapter(0);
-  self.error = self._adapter(1);
-  self.info  = self._adapter(2);
-  self.log   = self._adapter(3);
-  self.debug = self._adapter(4);
 
 
   return self;
