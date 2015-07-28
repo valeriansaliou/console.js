e5c4eb8a2ad795272b1dd65ada84a3d87defa33a
Valérian Saliou <valerian@valeriansaliou.name>
2015-07-28 20:19:22 +0200

    v1.1.0
    Signed-off-by: Valérian Saliou <valerian@valeriansaliou.name>

--- console.js
+++ console.js
@@ -23,39 +23,68 @@ var Console = (function () {
   self._console = self._available ? console : {};
 
 
-  /* Adapters */
-  self._adapter = function (level) {
-    if (!self._has) {
-      return function() {};
+  /* Methods */
+  self._warn = function (ns, value) {
+    if (self._has) {
+      console.warn(ns, value);
     }
+  };
 
+  self._error = function (ns, value) {
+    if (self._has) {
+      console.error(ns, value);
+    }
+  };
+
+  self._info = function (ns, value) {
+    if (self._has) {
+      console.info(ns, value);
+    }
+  };
+
+  self._log = function (ns, value) {
+    if (self._has) {
+      console.log(ns, value);
+    }
+  };
+
+  self._debug = function (ns, value) {
+    if (self._has) {
+      console.debug(ns, value);
+    }
+  };
+
+
+  /* Adapters */
+  self._adapter = function (level, ns, value) {
     var adapter = null;
+
     try {
       switch (level) {
         case 0:
-          adapter = console.warn; break;
+          adapter = self._warn;  break;
         case 1:
-          adapter = console.error; break;
+          adapter = self._error; break;
         case 2:
-          adapter = console.info; break;
+          adapter = self._info;  break;
         case 3:
-          adapter = console.log; break;
+          adapter = self._log;   break;
         case 4:
-          adapter = console.debug; break;
+          adapter = self._debug; break;
       }
     } catch (e) {
       adapter = function() {};
     }
 
-    return adapter.bind(self._console);
+    return adapter;
   };
 
 
   /* Methods */
-  self.warn = self._adapter(0);
+  self.warn  = self._adapter(0);
   self.error = self._adapter(1);
-  self.info = self._adapter(2);
-  self.log = self._adapter(3);
+  self.info  = self._adapter(2);
+  self.log   = self._adapter(3);
   self.debug = self._adapter(4);
 
 
